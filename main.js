f6fb3c68662fd3996fa1dbf500857fbf5a155779
pujasharma5431 <pujasharma5431@gmail.com>
2023-02-28 09:59:31 +0545

    DataTypes

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-// var. let, const
+// // var. let, const
 
-let age= 23;
-age=34;
-console.log(age);
+// let age= 23;
+// age=34;
+// console.log(age);
 
-const age1= 24;
+// const age1= 24;
 
-console.log(age1)
+// console.log(age1)
+
+// Data Types
+// Strings, Numbers, Boolean, null, undefined
+
+const name= 'Puja Sharma';
+let age3 = 23;
+const isCool= 'True';
+const rating = 4.7;
+const x= null;
+const y= undefined;
+let z;
+ console.log(typeof age3);
+ console.log(typeof name);
+ console.log(typeof rating);
+ console.log(typeof x);
+ console.log(typeof y);
+ console.log(typeof z);
+
+
