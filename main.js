ad091234280358a3d9ca68e8a9289dbbf5d43465
pujasharma5431 <pujasharma5431@gmail.com>
2023-02-28 10:17:04 +0545

    concatination

--- main.js
+++ main.js
@@ -11,19 +11,26 @@
 // Data Types
 // Strings, Numbers, Boolean, null, undefined
 
-const name= 'Puja Sharma';
-let age3 = 23;
-const isCool= true;
-const rating = 4.7;
-const x= null;
-const y= undefined;
-let z;
- console.log(typeof age3);
- console.log(typeof name);
- console.log(typeof rating);
- console.log(typeof x);
- console.log(typeof y);
- console.log(typeof z);
- console.log(typeof isCool);
+// const name= 'Puja Sharma';
+// let age3 = 23;
+// const isCool= true;
+// const rating = 4.7;
+// const x= null;
+// const y= undefined;
+// let z;
+//  console.log(typeof age3);
+//  console.log(typeof name);
+//  console.log(typeof rating);
+//  console.log(typeof x);
+//  console.log(typeof y);
+//  console.log(typeof z);
+//  console.log(typeof isCool);
+
+
+// Concatination
+let name =" Puja";
+let age= " 22";
+// console.log('My name is'+ name + 'and my age is' +  age);
+console.log(`My name is ${name} and my age is ${age}`);
 
 
