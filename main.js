feca5951951369d0052a452d8077dce679be7741
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-01 12:29:37 +0545

    Privitives and objects

--- main.js
+++ main.js
@@ -1,184 +1,195 @@
-// // var. let, const
+// // // var. let, const
 
-// let age= 23;
-// age=34;
-// console.log(age);
+// // let age= 23;
+// // age=34;
+// // console.log(age);
 
-// const age1= 24;
+// // const age1= 24;
 
-// console.log(age1)
+// // console.log(age1)
 
-// Data Types
-// Strings, Numbers, Boolean, null, undefined
+// // Data Types
+// // Strings, Numbers, Boolean, null, undefined
 
-// const name= 'Puja Sharma';
-// let age3 = 23;
-// const isCool= true;
-// const rating = 4.7;
-// const x= null;
-// const y= undefined;
-// let z;
-//  console.log(typeof age3);
-//  console.log(typeof name);
-//  console.log(typeof rating);
-//  console.log(typeof x);
-//  console.log(typeof y);
-//  console.log(typeof z);
-//  console.log(typeof isCool);
-
-
-// Concatination
-// let name =" Puja";
-// let age= " 22";
-// // console.log('My name is'+ name + 'and my age is' +  age);
-// console.log(`My name is ${name} and my age is ${age}`);
-// let s='Hello, Js, JavaScript'
-//  console.log(s.length);
-//  //  [Methods so need paranthesis]
-//  console.log(s.toUpperCase());
-//  console.log(s.substring(0,4))
-//  console.log(s.substring(0,4).toLowerCase());
-//  console.log(s.split(''))
-//  console.log(s.split(', '))
-
-// Operators
-
-// let a= 4;
-// let b= 7;
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-// console.log(a/b);
-// console.log(a%b)
-// console.log(--a);
-
-// Arithmetic Operators
-// + = Operator
-8
-// let x= 4;
-// x *=5;
-// console.log(x)
+// // const name= 'Puja Sharma';
+// // let age3 = 23;
+// // const isCool= true;
+// // const rating = 4.7;
+// // const x= null;
+// // const y= undefined;
+// // let z;
+// //  console.log(typeof age3);
+// //  console.log(typeof name);
+// //  console.log(typeof rating);
+// //  console.log(typeof x);
+// //  console.log(typeof y);
+// //  console.log(typeof z);
+// //  console.log(typeof isCool);
 
 
+// // Concatination
+// // let name =" Puja";
+// // let age= " 22";
+// // // console.log('My name is'+ name + 'and my age is' +  age);
+// // console.log(`My name is ${name} and my age is ${age}`);
+// // let s='Hello, Js, JavaScript'
+// //  console.log(s.length);
+// //  //  [Methods so need paranthesis]
+// //  console.log(s.toUpperCase());
+// //  console.log(s.substring(0,4))
+// //  console.log(s.substring(0,4).toLowerCase());
+// //  console.log(s.split(''))
+// //  console.log(s.split(', '))
 
-/*Multiline 
-comment
-*/
-// //  Arrays - variables that holds multiple values
+// // Operators
 
-// const fruits= ['Apple','Mango','Litchi'];
-// fruits[3]='oranges';
+// // let a= 4;
+// // let b= 7;
+// // console.log(a+b);
+// // console.log(a-b);
+// // console.log(a*b);
+// // console.log(a/b);
+// // console.log(a%b)
+// // console.log(--a);
 
-// console.log(fruits[0]);
-// fruits.push('pears');
-
-// fruits.unshift('strawberry');
-
-// fruits.pop();
-
-// console.log(fruits);
+// // Arithmetic Operators
+// // + = Operator
+// 8
+// // let x= 4;
+// // x *=5;
+// // console.log(x)
 
 
 
+// /*Multiline 
+// comment
+// */
+// // //  Arrays - variables that holds multiple values
 
-// // Arrays
-// let vechiles =['car','bus','scooter'];
-// vechiles[3]='Tuktuke';
-// vechiles.pop();
-// vechiles.push('hey')
-// vechiles.unshift('straw');
-// console.log(vechiles.indexOf('bus'));
-// conso
-// console.log(vechiles);
-// console.log(Array.isArray(vechiles));
+// // const fruits= ['Apple','Mango','Litchi'];
+// // fruits[3]='oranges';
+
+// // console.log(fruits[0]);
+// // fruits.push('pears');
+
+// // fruits.unshift('strawberry');
+
+// // fruits.pop();
+
+// // console.log(fruits);
 
 
 
 
+// // // Arrays
+// // let vechiles =['car','bus','scooter'];
+// // vechiles[3]='Tuktuke';
+// // vechiles.pop();
+// // vechiles.push('hey')
+// // vechiles.unshift('straw');
+// // console.log(vechiles.indexOf('bus'));
+// // conso
+// // console.log(vechiles);
+// // console.log(Array.isArray(vechiles));
 
 
-// Object Literals
 
-// const person={
-//     firstName:'Puja',
-//     LastName:'Sharma',
-//     age:30,
-//     hobbies:['music','dancing','movies'],
-//     address:{
-//         street: 'devinagar',
-//         city: 'Butwal',
-//     }
+
+
+
+// // Object Literals
+
+// // const person={
+// //     firstName:'Puja',
+// //     LastName:'Sharma',
+// //     age:30,
+// //     hobbies:['music','dancing','movies'],
+// //     address:{
+// //         street: 'devinagar',
+// //         city: 'Butwal',
+// //     }
+// // }
+// // person.email="Puja@gmail.com";
+// // console.log(person);
+// // console.log(person.LastName, person.firstName, person.address.street, person.hobbies[2]);
+
+// // Objects
+
+
+// // const todos =[
+
+// //     {
+// //         id: 1,
+// //         task: 'Do hW',
+// //         isCompleted: true
+// //     },
+
+// //     {
+
+// //         id:2,
+// //         task: 'Complete Js crash Course',
+// //         isCompleted: true
+// //     },
+// //     {
+// //         id:3,
+// //         task: 'Have food',
+// //         isCompleted: false
+// //     }
+// // ];
+// //    console.log(todos[2].task)
+
+// //    const todoJSON = JSON.stringify(todos)
+// //    console.log(todoJSON);
+
+// // For
+// // for(let i = 0; i<=3; i++){
+// //     console.log(`Loop number is ${i}`);
+// // }
+
+
+// // // While 
+// // let i = 0;
+// // while(i<5){
+// //   console.log(`while loop is ${i}`)
+// //   i++;s
+// // }
+
+// // for(let i=0; i<todos.length; i++){
+// //     console.log(todos[i].task);
+// // }
+
+
+
+// // forEach, map, filter
+
+
+// // todos.forEach(function(todo){
+// //     console.log(todo.task)
+// // })
+
+// // // Map
+// // todos.map(function(todo){
+// //     console.log(todo.id)
+// // })
+
+
+// const task ={
+//     'id': 1,
+//     'text': Complete,
+//     'isCompleted':true
+
 // }
-// person.email="Puja@gmail.com";
-// console.log(person);
-// console.log(person.LastName, person.firstName, person.address.street, person.hobbies[2]);
-
-// Objects
-
-
-const todos =[
-
-    {
-        id: 1,
-        task: 'Do hW',
-        isCompleted: true
-    },
-
-    {
-
-        id:2,
-        task: 'Complete Js crash Course',
-        isCompleted: true
-    },
-    {
-        id:3,
-        task: 'Have food',
-        isCompleted: false
-    }
-];
-//    console.log(todos[2].task)
-
-//    const todoJSON = JSON.stringify(todos)
-//    console.log(todoJSON);
-
-// For
-// for(let i = 0; i<=3; i++){
-//     console.log(`Loop number is ${i}`);
-// }
-
-
-// // While 
-// let i = 0;
-// while(i<5){
-//   console.log(`while loop is ${i}`)
-//   i++;s
-// }
-
-// for(let i=0; i<todos.length; i++){
-//     console.log(todos[i].task);
-// }
+// console.log(task['isCompleted']);
 
 
 
-// forEach, map, filter
-
-
-todos.forEach(function(todo){
-    console.log(todo.task)
-})
-
-// Map
-todos.map(function(todo){
-    console.log(todo.id)
-})
-
-
-
-
-
-
-
-
+const task={
+    "Puja":true,
+    "Sharma": false,
+    "Arjun": 22,
+    "Gautam":undefined
+}
+console.log(task['Arjun']);
 
 
 
