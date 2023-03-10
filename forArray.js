2046c0f72f7bafefd34243c29077515ee0cc5754
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-10 09:50:19 +0545

    foreach

---
+++ forArray.js
@@ -0,0 +1,15 @@
+// for loops in arrays
+
+let num=[1,34,22,31]
+// for(let i=0;i<num.length;i++){
+//     console.log(num[i]);
+// }
+
+// forEach loops
+
+num.forEach((element)=>{
+
+    console.log(element)
+    console.log(element*element)
+
+})
