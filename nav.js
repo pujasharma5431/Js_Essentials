603102b6fc2f12db49c8828a283ddf062cc1e347
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-15 09:28:46 +0545

    matches

--- nav.js
+++ nav.js
@@ -1,3 +1,6 @@
-let b=document.body
-console.log("First child of b id ", b.firstChild)
-console.log("First  element child of b id ", b.firstElementChild)
+// let b=document.body
+// console.log("First child of b id ", b.firstChild)
+// console.log("First  element child of b id ", b.firstElementChild   )
+
+let id1= document.getElementById("id1")
+console.log(id1.matches(".box"))
